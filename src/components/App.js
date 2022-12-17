c73dea48bb73fa01917082dc9b3ee7b131991866
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2022-12-17 14:40:09 +0530

    Update App.js
    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,6 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
-const App = () => {
-  return (
-    <div id="main">
-     <h1 data-ns-test-attribute="project-name">name</h1>
-      <p data-ns-test-attribute="project-description">description</p>
-    </div>
-  )
-}
-
+const App = () => {  return (    <div id="main">   
+                             <p>I am learning React. My life is getting better.</p>    </div>  )}
 
 export default App;
